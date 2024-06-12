ab63fc877be12aa76db5f68b9e8a9445aa4e42a1
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-06-12 02:30:08 +0200

    Add /me endpoint to accounts view

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 urlpatterns = [
+    path('me/', views.me, name='me'),
     path('signup/', views.signup, name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
