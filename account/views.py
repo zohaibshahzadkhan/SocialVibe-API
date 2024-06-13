897f4c77f45ddbcd63f5f6a37e4a3780f6f3d0a8
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-06-14 01:08:30 +0200

    Add friendshipRequest modal and endpoint to handle friendship requests

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .forms import SignupForm
+from .models import User, FriendshipRequest
+from .serializers import UserSerializer, FriendshipRequestSerializer
 
 @api_view(['GET'])
 def me(request):
@@ -25,13 +27,45 @@ def signup(request):
     })
 
     if form.is_valid():
-        user = form.save()
+        form.save()
     else:
         message = form.errors.as_json()
 
     return JsonResponse({'message': message}, safe=False)
 
+@api_view(['POST'])
+def send_friendship_request(request, pk):
+    user = User.objects.get(pk=pk)
+    
+    check1 = FriendshipRequest.objects.filter(created_for=request.user).filter(created_by=user)
+    check2 = FriendshipRequest.objects.filter(created_for=user).filter(created_by=request.user)
 
+    if not check1 or not check2:
+        FriendshipRequest.objects.create(created_for=user, created_by=request.user)
+        return JsonResponse({'message': 'friendship request created'})
+    else:
+        return JsonResponse({'message': 'request already sent'})
+
+
+@api_view(['GET'])
+def friends(request, pk):
+    user = User.objects.get(pk=pk)
+    requests = []
+
+    if user == request.user:
+        requests = FriendshipRequest.objects.filter(created_for=request.user, status=FriendshipRequest.SENT)
+
+    friends = user.friends.all()
+
+    return JsonResponse({
+        'user': UserSerializer(user).data,
+        'friends': UserSerializer(friends, many=True).data,
+        'requests': FriendshipRequestSerializer(requests, many=True).data
+    }, safe=False)
+
+
+@api_view(['POST'])
+def handle_request(request, pk, status):
     user = User.objects.get(pk=pk)
     friendship_request = FriendshipRequest.objects.filter(created_for=request.user).get(created_by=user)
     friendship_request.status = status
@@ -44,7 +78,5 @@ def signup(request):
     request_user = request.user
     request_user.friends_count = request_user.friends_count + 1
     request_user.save()
-
-    notification = create_notification(request, 'accepted_friendrequest', friendrequest_id=friendship_request.id)
 
     return JsonResponse({'message': 'friendship request updated'})
