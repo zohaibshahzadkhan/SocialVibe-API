206d27795a3f0c6650481e3012e2fdf46f913b7b
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-06-13 03:47:44 +0200

    Add Create post view

--- post/views.py
+++ post/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .models import Post
 from .serializers import PostSerializer
+from .forms import PostForm, AttachmentForm
 
 
 @api_view(['GET'])
@@ -11,3 +12,31 @@ def post_list(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['POST'])
+def post_create(request):
+    form = PostForm(request.POST)
+    attachment = None
+    attachment_form = AttachmentForm(request.POST, request.FILES)
+
+    if attachment_form.is_valid():
+        attachment = attachment_form.save(commit=False)
+        attachment.created_by = request.user
+        attachment.save()
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
+
+        if attachment:
+            post.attachments.add(attachment)
+
+        user = request.user
+        user.save()
+
+        serializer = PostSerializer(post)
+
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        return JsonResponse({'error': 'unable to create post...'})
