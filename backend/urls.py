51df9a3461d18c8cb4c7f09debb8cb75f934f4bf
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-06-29 23:30:06 +0200

    Add API root endpoint with welcome message

--- backend/urls.py
+++ backend/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import api_root 
 
 urlpatterns = [
+    path('', api_root),
     path('api/', include('account.urls')),
     path('api/posts/', include('post.urls')),
     path('api/search', include('search.urls')),
---
+++ backend/views.py
@@ -0,0 +1,15 @@
+# views.py
+
+from django.http import JsonResponse
+
+
+def api_root(request):
+    """
+    API root endpoint.
+
+    Returns a JSON response with a welcome message and status code 200.
+    
+    """
+    return JsonResponse(
+        {"status": 200, "message": "Welcome to Socialvibe Django REST Framework API"}
+    )
